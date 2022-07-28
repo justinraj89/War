824b90722806d283be7ac413fa3f273f5772412f
justinraj89 <rajewskijustin@gmail.com>
2022-07-27 21:30:48 -0700

    fixed War function bug

--- js/app.js
+++ js/app.js
@@ -11,16 +11,12 @@ let cpuCardIndex;
 let playerCardIndex;
 let playerCard;
 let cpuCard;
-
 let playerWarCards;
 let cpuWarCards;
 let warModeEnabled = false;
 
 
-// create elements for the current cards remaining values
-// you may have to seperate the number from the current <div>
-// and then cache them to in order to manipulate them with DOM
-
+//--------------- DOM Elements
 const drawButton = document.querySelector('.draw');
 const startButton = document.querySelector('.start');
 const warButton = document.querySelector('.war');
@@ -30,7 +26,6 @@ const cpuCardsRemaining = document.querySelector('.cpu-cards');
 let playerCardImage = document.querySelector('#p-card');
 let cpuCardImage = document.querySelector('#c-card');
 const winnerNotification = document.querySelector('.winnerNotification');
-
 let playerWarCardFaceDown = document.querySelector('#player-warCardDown');  // Tied to the war function, only revelaed during 'War'
 let cpuWarCardFaceDown = document.querySelector('#cpu-warCardDown');        // Tied to the war function, only revelaed during 'War'
 let playerWarCardFaceUp = document.querySelector('#player-warCardUp');      // Tied to the war function, only revelaed during 'War'
@@ -49,8 +44,12 @@ function init() {
 }
 
 function restartGame() {
+    if(warModeEnabled) {
+        warCleanup();
+    }
     playerCardImage.classList.add('hide');
     cpuCardImage.classList.add('hide');
+    winnerNotification.innerText = '';
     init();
     startGame();
 }
@@ -79,74 +78,73 @@ function drawCard() {
     playerCardImage.classList.remove('hide');
     cpuCardImage.classList.remove('hide');
         
-    playerCard = playerCards[playerCardIndex];   // in the init(), playerCardIndex and cpuCardIndex both set to 0
-    cpuCard = cpuCards[cpuCardIndex];           // so playerCard and cpuCard indexes both start at 0 to begin with.
+    playerCard = playerCards.shift()  // shift removes an element from beginning of array.
+    cpuCard = cpuCards.shift()      
 
     console.log("PLAYER CARD INDEX and Value", playerCardIndex, "=", playerCard.value,
     "CPU CARD INDEX and Value", cpuCardIndex, "=", cpuCard.value);
 
+    winnerNotification.innerText = '';
+
     if (playerCard.value > cpuCard.value) {   // The conditional statement checks for the higher value (values assigned to each card object)
+        playerCards.push(playerCard);            // the winner pushes the losers card into their deck
         playerCards.push(cpuCard);            // the winner pushes the losers card into their deck
-        cpuCards.splice(cpuCardIndex,1);
-        winnerNotification.innerText = 'Player Wins!'       // splice parameters are.. (start, how many elements to remove. So in this case, the current card)
+        winnerNotification.innerText = 'Player Wins!';                               
+        render();
     }                                                                                
     else if (playerCard.value < cpuCard.value) {
+        cpuCards.push(cpuCard);
         cpuCards.push(playerCard);
-        playerCards.splice(playerCardIndex,1);
         winnerNotification.innerText = 'CPU Wins!'
+        render();
 
     } else {
+        renderWar();
         console.log('TIE');                   // In the case of a tie, or "War", the warButton is revealed, and the draw button is hidden.
         warButton.classList.remove ("hide");  // the Tied cards remain on the screen.
-        drawButton.classList.add('hide');      
-        }
-    
-    cpuCardIndex++           // after the conditional statements have been checked, the playerCardIndex and cpuCardIndex both get incremented by 1       
-    playerCardIndex++
-    
-
-    console.log("Indexes after increment", cpuCardIndex,playerCardIndex)
-
-    if(cpuCardIndex >= (cpuCards.length - 1 )) {  // This conditional statement checks the current card index and makes sure it does not go over the                                               // length of the cards remaining in deck (player, or CPU). Also checks for a Win.
-        if (cpuCards.length === 52) {
-            alert("Computer WINS!")
-            return;     
-        } 
-        console.log("RESETTING CPU INDEX") 
-        cpuCardIndex = Math.floor(Math.random() * cpuCards.length - 1 );  // this resets the card index to a random number upon reset (avoid never ending game)
+        drawButton.classList.add('hide');   
+        winnerNotification.innerText = "WAR!";
+       
     }
-
-    if(playerCardIndex >= (playerCards.length - 1)) {
-        if (playerCards.length === 52) {
-            alert("PLAYER WINS!")
-            return;       
-        } 
-        console.log("RESETTING PLAYER INDEX")
-        playerCardIndex = Math.floor(Math.random() * playerCards.length - 1);
-    }
-    render();
-}
-  
-function getRandomCard(cards) {
-    return cards[(Math.floor(Math.random() * cards.length-1))];  //This is just a function that will pick a random card, and put into an array
+    
+    if (cpuCards.length === 52) {
+        drawButton.classList.add('hide');   
+        winnerNotification.innerText = 'CPU Wins Gane!'
+        return;     
+    } 
+    else if (playerCards.length === 52) {
+        drawButton.classList.add('hide');   
+        winnerNotification.innerText = 'Player Wins Gane!'
+        return;       
+    } 
+    // render()
 }
 
-
-
-function war() {             // // this function is tied to the warButton click eventListener . Created objects for players war cards, and CPUs war cards.
+function war() {   // // this function is tied to the warButton click eventListener . Created objects for players war cards, and CPUs war cards.
+    if(playerCards.length <= 3) {
+        drawButton.classList.add('hide');
+        warButton.classList.add('hide');   
+        winnerNotification.innerText = 'CPU Wins the Game!'
+        return;     
+    } 
+    else if(cpuCards.length <= 3) {
+        warButton.classList.add('hide');
+        drawButton.classList.add('hide');   
+        winnerNotification.innerText = 'Player Wins the Game!'
+        return;     
+    }
+    
     warModeEnabled = true;  
 
     playerWarCards = {
-        faceUpCard: getRandomCard(playerCards),  // uses the above ^ getRandomCard() to generate cards for war
-        faceDownCard: getRandomCard(playerCards) // one card is dealed down, the other dealed up, whoevers faceUpCard value is higher wins both cards.
+        faceDownCard: playerCards.shift(),  // one card is dealed down, the other dealed up, whoevers faceUpCard value is higher wins both cards.
+        faceUpCard: playerCards.shift()  
     };
-    console.table(playerWarCards)
-    
+
     cpuWarCards = {
-        faceUpCard: getRandomCard(cpuCards), 
-        faceDownCard: getRandomCard(cpuCards)
+        faceDownCard: cpuCards.shift(),  // one card is dealed down, the other dealed up, whoevers faceUpCard value is higher wins both cards.
+        faceUpCard: cpuCards.shift()  
     };
-    console.table(cpuWarCards) 
 
     playerWarCardFaceUp.src = playerWarCards.faceUpCard.imageUrl;  // sets the images of the faceUp cards to their imageURL
     cpuWarCardFaceUp.src = cpuWarCards.faceUpCard.imageUrl;        // warFaceCardUp variables have a CSS 'hide' applied
@@ -162,36 +160,33 @@ function war() {             // // this function is tied to the warButton click 
     warButton.classList.add("hide");  // warButton is hidden after it is clicked, and drawCard button comes back
     drawButton.classList.remove("hide");  // draw card button comes back, but is not clicked during war, when you click it, it resets back to normal gameplay
 
-    if(playerWarCards.faceUpCard.value > cpuWarCards.faceUpCard.value) {  //Pushing the three cards, warFaceUp warFaceDown, and Tie card
-        playerCards.push(cpuWarCards.faceUpCard);
+    if(playerWarCards.faceUpCard.value > cpuWarCards.faceUpCard.value) {  
+        playerCards.push(cpuWarCards.faceUpCard); 
         playerCards.push(cpuWarCards.faceDownCard);
-        playerCards.push(cpuCard); // double check if this is right
-
-        let spliceIndex = cpuCards.findIndex(card => card.face === cpuWarCards.faceDownCard.face)  
-        cpuCards.splice(spliceIndex, 1);
-        spliceIndex = cpuCards.findIndex(card => card.face === cpuWarCards.faceUpCard.face)
-        cpuCards.splice(spliceIndex, 1);
-        cpuCards.splice(cpuCardIndex,1);  // <--- THIS IS WHERE SOMETHING IS WRONG I THINK
-
-
+        playerCards.push(playerWarCards.faceUpCard);
+        playerCards.push(playerWarCards.faceDownCard); 
+        playerCards.push(playerCard);  
+        playerCards.push(cpuCard); 
         render();
-        setTimeout(() => alert("PLAYER WINS!"), 500);
+        winnerNotification.innerText = 'Player Wins War!'
     }
     else if (playerWarCards.faceUpCard.value < cpuWarCards.faceUpCard.value) {
+        cpuCards.push(cpuWarCards.faceUpCard);
+        cpuCards.push(cpuWarCards.faceDownCard);
         cpuCards.push(playerWarCards.faceUpCard);
         cpuCards.push(playerWarCards.faceDownCard);
-        cpuCards.push(playerCard);  // double check if this is right
-        
-        let spliceIndex = playerCards.findIndex(card => card.face === playerWarCards.faceDownCard.face)
-        playerCards.splice(spliceIndex, 1);
-        spliceIndex = playerCards.findIndex(card => card.face === playerWarCards.faceUpCard.face)
-        playerCards.splice(spliceIndex, 1);
-        playerCards.splice(playerCardIndex,1); // <-- THIS IS WHERE SOMETHING IS WRONG I THINK
-       
+        cpuCards.push(playerCard);
+        cpuCards.push(cpuCard); 
         render();
-        setTimeout(() => alert("COMPUTER WINS!"), 500);
+        winnerNotification.innerText = 'CPU Wins War!'
     } 
+    else {
+        war();
+    }
 
+    console.table("PC AFTER WAR",playerCards.length)
+    
+    console.table("CC AFTER WAR",cpuCards.length)
 
 }
 
@@ -200,6 +195,11 @@ function render(){   // This will need to render the currentCard image to the sc
     
     playerCardsRemaining.innerText = playerCards.length;
     cpuCardsRemaining.innerText = cpuCards.length;
+    playerCardImage.src = playerCard.imageUrl;
+    cpuCardImage.src = cpuCard.imageUrl;
+}
+
+function renderWar() {
     playerCardImage.src = playerCard.imageUrl;
     cpuCardImage.src = cpuCard.imageUrl;
 }
