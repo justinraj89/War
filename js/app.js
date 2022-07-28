73d42a3d4d98d2c7e1d459a79456409c2f59e459
justinraj89 <rajewskijustin@gmail.com>
2022-07-28 10:43:09 -0700

    Double War bug patched

--- js/app.js
+++ js/app.js
@@ -31,8 +31,6 @@ let cpuWarCardFaceDown = document.querySelector('#cpu-warCardDown');        // T
 let playerWarCardFaceUp = document.querySelector('#player-warCardUp');      // Tied to the war function, only revelaed during 'War'
 let cpuWarCardFaceUp = document.querySelector('#cpu-warCardUp');            // Tied to the war function, only revealed during 'War'
 
-const simulateButton = document.querySelector('.simulate');
-
 
 init();
 
@@ -41,6 +39,13 @@ function init() {
     cpuCards = shuffledDeck.slice (26 , 52);
     cpuCardIndex = 0;
     playerCardIndex = 0;
+}
+
+function startGame () {
+    startButton.classList.add("hide");  // when you click 'start game' button it is hidden
+    drawButton.classList.remove("hide");   // and then 'draw card' button is revealed
+    playerCardsRemaining.innerText = 26;  // sets player and CPU scores to 26
+    cpuCardsRemaining.innerText = 26;
 }
 
 function restartGame() {
@@ -54,13 +59,6 @@ function restartGame() {
     startGame();
 }
 
-function startGame () {
-    startButton.classList.add("hide");  // when you click 'start game' button it is hidden
-    drawButton.classList.remove("hide");   // and then 'draw card' button is revealed
-    playerCardsRemaining.innerText = 26;  // sets player and CPU scores to 26
-    cpuCardsRemaining.innerText = 26;
-}
-
 function warCleanup() {
     playerWarCardFaceDown.classList.add("hide");  // this functions purpose is to hide all the extra cards
     cpuWarCardFaceDown.classList.add("hide");     // that get displayed when war takes place. It is put at the
@@ -70,7 +68,6 @@ function warCleanup() {
 }
 
 function drawCard() {
-
     if (warModeEnabled) { 
         warCleanup() 
     }
@@ -81,10 +78,9 @@ function drawCard() {
     playerCard = playerCards.shift()  // shift removes an element from beginning of array.
     cpuCard = cpuCards.shift()      
 
-    console.log("PLAYER CARD INDEX and Value", playerCardIndex, "=", playerCard.value,
-    "CPU CARD INDEX and Value", cpuCardIndex, "=", cpuCard.value);
+    console.log(playerCard);
 
-    winnerNotification.innerText = '';
+    winnerNotification.innerText = '';  
 
     if (playerCard.value > cpuCard.value) {   // The conditional statement checks for the higher value (values assigned to each card object)
         playerCards.push(playerCard);            // the winner pushes the losers card into their deck
@@ -96,38 +92,35 @@ function drawCard() {
         cpuCards.push(cpuCard);
         cpuCards.push(playerCard);
         winnerNotification.innerText = 'CPU Wins!'
-        render();
-
+        render();    
     } else {
         renderWar();
         console.log('TIE');                   // In the case of a tie, or "War", the warButton is revealed, and the draw button is hidden.
         warButton.classList.remove ("hide");  // the Tied cards remain on the screen.
         drawButton.classList.add('hide');   
         winnerNotification.innerText = "WAR!";
-       
     }
-    
+
     if (cpuCards.length === 52) {
         drawButton.classList.add('hide');   
-        winnerNotification.innerText = 'CPU Wins Gane!'
+        winnerNotification.innerText = 'CPU Wins the Game!'
         return;     
     } 
     else if (playerCards.length === 52) {
         drawButton.classList.add('hide');   
-        winnerNotification.innerText = 'Player Wins Gane!'
+        winnerNotification.innerText = 'Player Wins the Game!'
         return;       
     } 
-    // render()
 }
 
 function war() {   // // this function is tied to the warButton click eventListener . Created objects for players war cards, and CPUs war cards.
-    if(playerCards.length <= 3) {
+    if(playerCards.length <= 2) {
         drawButton.classList.add('hide');
         warButton.classList.add('hide');   
         winnerNotification.innerText = 'CPU Wins the Game!'
         return;     
     } 
-    else if(cpuCards.length <= 3) {
+    else if(cpuCards.length <= 2) {
         warButton.classList.add('hide');
         drawButton.classList.add('hide');   
         winnerNotification.innerText = 'Player Wins the Game!'
@@ -135,6 +128,14 @@ function war() {   // // this function is tied to the warButton click eventListe
     }
     
     warModeEnabled = true;  
+
+    playerWarCardFaceDown.classList.remove("hide");   // these are all hidden during normal gameplay (when its not war)
+    cpuWarCardFaceDown.classList.remove("hide");      // when war occurs, all are rendered onto screen. Removing 'hide' CSS class.
+    playerWarCardFaceUp.classList.remove("hide");
+    cpuWarCardFaceUp.classList.remove("hide");
+
+    warButton.classList.add("hide");  // warButton is hidden after it is clicked, and drawCard button comes back
+    drawButton.classList.remove("hide");  // draw card button comes back, but is not clicked during war, when you click it, it resets back to normal gameplay
 
     playerWarCards = {
         faceDownCard: playerCards.shift(),  // one card is dealed down, the other dealed up, whoevers faceUpCard value is higher wins both cards.
@@ -149,16 +150,10 @@ function war() {   // // this function is tied to the warButton click eventListe
     playerWarCardFaceUp.src = playerWarCards.faceUpCard.imageUrl;  // sets the images of the faceUp cards to their imageURL
     cpuWarCardFaceUp.src = cpuWarCards.faceUpCard.imageUrl;        // warFaceCardUp variables have a CSS 'hide' applied
 
-    playerWarCardFaceDown.classList.remove("hide");   // these are all hidden during normal gameplay (when its not war)
-    cpuWarCardFaceDown.classList.remove("hide");      // when war occurs, all are rendered onto screen. Removing 'hide' CSS class.
-    playerWarCardFaceUp.classList.remove("hide");
-    cpuWarCardFaceUp.classList.remove("hide");
-
     // console.log(playerWarCards.faceUpCard);
     // console.log(cpuWarCards.faceUpCard);
 
-    warButton.classList.add("hide");  // warButton is hidden after it is clicked, and drawCard button comes back
-    drawButton.classList.remove("hide");  // draw card button comes back, but is not clicked during war, when you click it, it resets back to normal gameplay
+    
 
     if(playerWarCards.faceUpCard.value > cpuWarCards.faceUpCard.value) {  
         playerCards.push(cpuWarCards.faceUpCard); 
@@ -181,13 +176,14 @@ function war() {   // // this function is tied to the warButton click eventListe
         winnerNotification.innerText = 'CPU Wins War!'
     } 
     else {
-        war();
+        winnerNotification.innerText = 'Double War, Players receive cards back';
+        playerCards.push(playerWarCards.faceUpCard);
+        playerCards.push(playerWarCards.faceDownCard); 
+        playerCards.push(playerCard); 
+        cpuCards.push(cpuWarCards.faceUpCard);
+        cpuCards.push(cpuWarCards.faceDownCard);
+        cpuCards.push(cpuCard); 
     }
-
-    console.table("PC AFTER WAR",playerCards.length)
-    
-    console.table("CC AFTER WAR",cpuCards.length)
-
 }
 
 
